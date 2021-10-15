032b6aa9292b1a2908ebec9af7b5e2859888c829
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-14 23:30:37 -0300

    first commit

---
+++ SwiftUI-Todos-TCA/SwiftUI_Todos_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Todos_TCAApp.swift
+//  SwiftUI-Todos-TCA
+//
+//  Created by bruno on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Todos_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
