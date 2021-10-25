5d894c5d1237d9983ef3ec7db0ed9346b5247b32
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-24 23:21:32 -0300

    added a unit test file to covert general app functionality

---
+++ SwiftUI-Todos-TCATests/TodosTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodosTests.swift
+//  SwiftUI-Todos-TCATests
+//
+//  Created by bruno on 24/10/21.
+//
+
+import XCTest
+@testable import SwiftUI_Todos_TCA
+
+final class TodosTests: XCTestCase {
+    
+    func testCompletingTodo() {
+        
+    }
+}
