ffa4908ea8c63cdf66fa909745af0de1137ce285
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-28 00:19:08 -0300

    removed unused code

--- SwiftUI-Todos-TCA/Todo.swift
+++ SwiftUI-Todos-TCA/Todo.swift
@@ -13,23 +13,3 @@ struct Todo: Equatable, Identifiable {
     var description = ""
     var isComplete = false
 }
-
-extension IdentifiedArray where ID == Todo.ID, Element == Todo {
-    static let mock: Self = [
-        Todo(
-            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEDDEADBEEF")!,
-            description: "Check Mail",
-            isComplete: false
-        ),
-        Todo(
-            id: UUID(uuidString: "CAFEBEEF-CAFE-BEEF-CAFE-BEEFCAFEBEEF")!,
-            description: "Buy Milk",
-            isComplete: false
-        ),
-        Todo(
-            id: UUID(uuidString: "D00DCAFE-D00D-CAFE-D00D-CAFED00DCAFE")!,
-            description: "Call Mom",
-            isComplete: true
-        ),
-    ]
-}
