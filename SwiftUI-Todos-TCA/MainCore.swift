9e31bc90053f39e54f205c2e7b0a3d3dcdf62d2b
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-27 23:11:33 -0300

    removed deadcode

--- SwiftUI-Todos-TCA/MainCore.swift
+++ SwiftUI-Todos-TCA/MainCore.swift
@@ -22,25 +22,6 @@ struct AppEnvironment {
     var uuid: () -> UUID
 }
 
-//let appReducer: Reducer<AppState, AppAction, AppEnvironment> = todoReducer.forEach(
-//    state: \AppState.todos,
-//    action: /AppAction.todo(index:action:),
-//    environment: { _ in TodoEnvironment() }
-//)
-//.debug()
-
-//    Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
-//    switch action {
-//    case .todoCheckboxTapped(index: let index):
-//        state.todos[index].isComplete.toggle()
-//        return .none
-//    case .todoTextFieldChanged(index: let index, text: let text):
-//        state.todos[index].description = text
-//        return .none
-//    }
-//}
-//.debug()
-
 let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
     todoReducer.forEach(state: \.todos,
                         action: /AppAction.todo(index:action:),
